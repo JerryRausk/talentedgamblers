63dcd24826b0fe0553584b4a043f78e91f63b1ee
Jerry <jerry.rausk@gmail.com>
2023-12-17 21:07:39 +0100

    remove boilerplate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,33 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HRRRMR so fast
-    </p>
+  
+  <div style="align-items: center; justify-content: center; height: 70vh">
+    <h1>Meatheads 2023</h1>
+    <h2>Coming soon...</h2>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+table {
+  width: 100%;
+}
+.table-wrapper {
+  min-width: 20rem;
+  max-width: 40rem;
+  margin-top: 1rem;
+  border: 1px solid black;
+  border-radius: 8px;
+}
+th, td {
+  padding: 0.25rem;
+  vertical-align: middle;
+}
+.profile-pic {
+  height: 3rem;
+  width: 3rem;
+  border-radius: 100%;
 }
 </style>
